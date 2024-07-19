e9adcfc4ffbbba367c6eb1802dc022533c0f4147
Bin Chen <48148011+Guaishou74851@users.noreply.github.com>
2024-07-19 17:02:04 +0800

    Delete code directory

--- code/test.sh
+++
@@ -1,7 +0,0 @@
-# python -u test.py --gpu_list=1 --data_type=20240127_flureoscent_microsphere --gq=25 > scalable_FM_20240204_25.txt &
-# python -u test.py --gpu_list=2 --data_type=20240127_flureoscent_microsphere --gq=30 > scalable_FM_20240204_30.txt &
-# python -u test.py --gpu_list=0 --data_type=20240127_potato_tuber --gq=30 > scalable_PT_20240204_30.txt &
-# python -u test.py --gpu_list=3 --data_type=20240127_potato_tuber --gq=39 > scalable_PT_20240204_39.txt &
-# python -u test.py --gpu_list=0 --data_type=20240127_potato_tuber --gq=30 > /dev/null &
-python -u test.py --gpu_list=2 --data_type=20240127_potato_tuber --gq=30 > /dev/null &
-python -u test.py --gpu_list=3 --data_type=20240127_potato_tuber --gq=39 > /dev/null &
--- code/train.sh
+++
@@ -1,4 +0,0 @@
-nohup python -u train.py --gpu_list=0 --data_type=flureoscent_microsphere --gq=40 > fm_40_g0.txt &
-nohup python -u train.py --gpu_list=1 --data_type=flureoscent_microsphere --gq=50 > fm_50_g1.txt &
-nohup python -u train.py --gpu_list=2 --data_type=potato_tuber --gq=30 > pt_30_g2.txt &
-nohup python -u train.py --gpu_list=3 --data_type=potato_tuber --gq=39 > pt_39_g3.txt &
